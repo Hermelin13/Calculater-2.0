acc342b4f673d141f4532e8f1a6d564fd19fa503
Hermelín <adamdaliborj@gmail.com>
2023-10-22 16:56:32 +0200

    pridany buttony do kalkulacky

--- Form1.cs
+++ Form1.cs
@@ -10,12 +10,16 @@ using System.Windows.Forms;
 
 namespace Calculater
 {
-    public partial class Form1 : Form
+    public partial class FITCalculator : Form
     {
-        public Form1()
+        public FITCalculator()
         {
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
