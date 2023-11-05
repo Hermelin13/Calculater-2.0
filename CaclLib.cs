085bb29999eded28d93d0c2c6478f2daf5002f74
Hermelín <adamdaliborj@gmail.com>
2023-11-05 17:54:37 +0100

    sexy uprava

--- CaclLib.cs
+++ CaclLib.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CalcLib
+namespace Calculater
 {
-    public class mathematics
+    public class CalcLib
     {
 
     }
